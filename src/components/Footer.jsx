7b8e49e1b77e265ff43bb371f64f4b86b26ec976
munishmangla98 <munishmangla98@gmail.com>
2024-06-30 17:09:31 +0530

    linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,62 +4,63 @@ import Writebolgs from "./Writebolgs"
 function Footer() {
   return (
     <>
-    <div>
-      <hr />
-      <footer className="footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white">
-        <nav className="grid grid-flow-col gap-4">
-        <a href="/">Home</a>
+      <div>
+        <hr />
+        <footer className="footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white">
+          <nav className="grid grid-flow-col gap-4">
+            <a href="/">Home</a>
             <a href="/Allblogs">Blogs</a>
             <a href="/UserProfile">User Profile</a>
             <a onClick={() => document.getElementById("my_modal_4").showModal()}>Write Blogs</a>
-                <Writebolgs />
-            
-        </nav>
-        <nav>
-          <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a  href="https://youtu.be/DfQ63PRcgkQ?si=_1ttcmmcUlxwUBnv" target="_blank">
+            <Writebolgs />
 
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
-          </div>
-        </nav>
-        <aside>
-          {/* <p>Copyright © 2024 - All right reserved by Munish Mangla</p> */}
-          <p>Copyright © 2024 - All right reserved by Lucid Growth</p>
+          </nav>
+          <nav>
+            <div className="grid grid-flow-col gap-4">
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M22.23 0H1.77C.79 0 0 .78 0 1.75v20.5C0 23.22.79 24 1.77 24h20.5c.98 0 1.77-.78 1.77-1.75V1.75C24 .78 23.21 0 22.23 0zM7.08 20.5H3.56V9h3.52v11.5zM5.32 7.65c-1.12 0-1.82-.75-1.82-1.69 0-.96.72-1.69 1.85-1.69 1.12 0 1.82.73 1.83 1.69-.01.94-.71 1.69-1.86 1.69zM20.5 20.5h-3.51v-5.5c0-1.38-.49-2.32-1.72-2.32-.94 0-1.5.65-1.74 1.27-.09.22-.11.53-.11.84v5.71h-3.51s.05-9.28 0-10.24h3.51v1.45c.47-.73 1.3-1.76 3.16-1.76 2.3 0 4.03 1.49 4.03 4.69v5.86z"></path>
 
-        </aside>
-      </footer>
-    </div>
+                </svg>
+              </a>
+              <a href="https://youtu.be/DfQ63PRcgkQ?si=_1ttcmmcUlxwUBnv" target="_blank">
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                </svg>
+              </a>
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                </svg>
+              </a>
+            </div>
+          </nav>
+          <aside>
+            {/* <p>Copyright © 2024 - All right reserved by Munish Mangla</p> */}
+            <p>Copyright © 2024 - All right reserved by Lucid Growth</p>
+
+          </aside>
+        </footer>
+      </div>
     </>
   );
 }
